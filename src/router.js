bb8d93206315314237284fc4b766f12fd3610c68
Lina Jamal <linahjamal89@gmail.com>
2020-02-11 16:02:36 +0200

    makeAutoComplateHandler related #6

--- src/router.js
+++ src/router.js
@@ -1,7 +1,25 @@
 
+const fs = require('fs');
+
+const path = require('path');
+
 const router = (request, response) => {
   const endpoint = request.url;
-  console.log(endpoint);
+  // cnsole.log(endpoint);
+
+  if (endpoint.includes('cai')) {
+    const pathjson = path.join(__dirname, 'city.json');
+    fs.readFile(pathjson, (error, file) => {
+      if (error) {
+        response.writeHead(500, { 'Content-Type': 'text/html' });
+      } else {
+        const data = JSON.parse(file);
+        const searchValue = 'cai'.charAt(0).toUpperCase() + 'cai'.slice(1);
+        const result = data.filter((e) => e.includes(searchValue));
+        console.log(result);
+      }
+    });
+  }
 };
 
 module.exports = router;
