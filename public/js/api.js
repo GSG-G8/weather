9773f3011496c4ec66c5dccd53b2b96a4c2b9269
mariamisa <maryaam1998@hotmail.com>
2020-02-11 15:27:22 +0200

    add xhr response

---
+++ public/js/api.js
@@ -0,0 +1,14 @@
+
+const urlweather = `https://api.openweathermap.org/data/2.5/weather?lat=31.5&lon=34.5&appid=${id}&units=metric`;
+const xhr = (url, callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let data = xhr.responseText;
+      data = JSON.parse(data);
+      callback(data);
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+};
