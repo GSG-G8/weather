0b0d119ab1eccac46a2848186400fe396e46ff21
mariamisa <maryaam1998@hotmail.com>
2020-02-11 15:31:45 +0200

    fix eslint

--- public/js/api.js
+++ public/js/api.js
@@ -1,5 +1,5 @@
 const urlweather = `https://api.openweathermap.org/data/2.5/weather?lat=31.5&lon=34.5&appid=${id}&units=metric`;
-const xhr = (url, callback) => {
+const request = (url, callback) => {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = () => {
     if (xhr.readyState === 4 && xhr.status === 200) {
@@ -8,6 +8,6 @@ const xhr = (url, callback) => {
       callback(data);
     }
   };
-  xhr.open("GET", url, true);
+  xhr.open('GET', url, true);
   xhr.send();
 };
