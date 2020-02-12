1bbbfcb69060823fb0fa6040d6fe208fc1497419
Lina Ebeid <linaebe0@gmail.com>
2020-02-12 11:01:09 +0200

    modifiy handle functions Relates #9

---
+++ src/handle/handleSearch.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const qs = require('querystring');
+
+
+module.exports = (request, response) => {
+  const endpoint = request.url;
+  const searchValue = qs.parse(endpoint.split('?')[1]);
+  const name = searchValue.q;
+  const pathjson = path.join(__dirname, '..', 'city.json');
+  fs.readFile(pathjson, (error, file) => {
+    if (error) {
+      response.writeHead(500, { 'Content-Type': 'text/html' });
+      response.end('<h1>Internal Server Error</h1>');
+    } else {
+      const data = JSON.parse(file);
+      const result = data.filter((e) => e.toLowerCase().startsWith(name)).slice(0, 20);
+      response.end(JSON.stringify(result));
+    }
+  });
+};
--- src/router.js
+++ src/router.js
@@ -1,25 +1,16 @@
+const handleMain = require('./handle/handleHome');
+const handlePublic = require('./handle/handlePublic');
+const handleSearch = require('./handle/handleSearch');
 
-const fs = require('fs');
-const qs = require('querystring');
-const path = require('path');
 
 const router = (request, response) => {
   const endpoint = request.url;
-  if (endpoint.includes('/cities?q=')) {
-    const searchValue = qs.parse(endpoint.split('?')[1]);
-    const name = searchValue.q;
-    const pathjson = path.join(__dirname, 'city.json');
-    fs.readFile(pathjson, (error, file) => {
-      if (error) {
-        response.writeHead(500, { 'Content-Type': 'text/html' });
-        response.end('internal server error');
-      } else {
-        const data = JSON.parse(file);
-        // const cityToSEarch = name.charAt(0).toUpperCase() + name.slice(1);
-        const result = data.filter((e) => e.toLowerCase().startsWith(name)).slice(0, 20);
-        response.end(JSON.stringify(result));
-      }
-    });
+  if (endpoint === '/') {
+    handleMain(request, response);
+  } else if (endpoint.includes('public')) {
+    handlePublic(request, response);
+  } else if (endpoint.includes('/cities?q=')) {
+    handleSearch(request, response);
   }
 };
 
